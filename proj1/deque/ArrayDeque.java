7c2505aa826bb1833de20ee6328d4114758ddefc
Sinclair19 <zsq0219@outlook.com>
2023-05-28 14:54:22 +0800

    week3: project 1: remove unused code

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -22,54 +22,12 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
         items = a;
     }
 
-    private void resizer(int capacity) {
-        T[] a = (T[]) new Object[start + capacity];
-        System.arraycopy(items, start, a, start, size);
-        items = a;
-    }
-
-    private void resizel(int capacity) {
-        int rightSpace = items.length + 1 - start; // lenght count from 1, start count from 0
-        int resizedSize = capacity + rightSpace;
-        T[] a = (T[]) new Object[resizedSize];
-        System.arraycopy(items, start, a, capacity, size);
-
-        start = capacity;
-        items = a;
-    }
-
     private void dresize() {
         if ((double) size / items.length < 0.25 && items.length > 8) {
             resize(size * 4);
         }
     }
 
-    private void dresizer() {
-        int length = items.length - start;
-        if ((double) size / (length) < 0.25) {
-            resizer(size * 4); // let right size / length = 0.25
-        }
-    }
-
-    private void dresizel() {
-        int length = start + size;
-        if ((double) size / (length) < 0.25) {
-            resizel(size * 4); // let right size / length = 0.25
-        }
-    }
-
-    /**
-    private void resizer(int capacity, int position) {
-        // overload with position argument
-        T[] temp = (T[]) new Object[items.length + 1]; // to allow at least one position to be empty
-        if ((double) size / items.length > 0.25){
-            temp = (T[]) new Object[capacity + 1];
-        }
-        System.arraycopy(items, 0, temp, 0, position); // separately copy
-        System.arraycopy(items, position, temp, position + 1, size - position);
-        items = temp;
-    }
-     */
     private void checkFirstNoNull() {
         int half = (int) Math.round(items.length / 2.0);
         while (start < half && items[start] == null) {
