ff268040484fe8d60c9eb15b61a7d1b612abc92f
Sinclair19 <zsq0219@outlook.com>
2023-05-31 18:06:04 +0800

    week4: lab 4

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -8,6 +8,13 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
+        return a.equals(b);
+    }
+    // for Integer, if the value is between -128 and 127,
+    // it will use the cached pool and this is true only when auto-boxing
+    /**
+    public static boolean isSameNumber(int a, int b) {
         return a == b;
     }
+     */
 }
